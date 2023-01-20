627788bec74b2ce04ff8a1ccf78e22b2950e7523
SrNandu <octaula@gmail.com>
2023-01-19 23:40:57 -0300

    Red de petri implementada

---
+++ Rdp.java
@@ -0,0 +1,63 @@
+public class Rdp {
+
+    private int[][] matriz;
+    private int[] estado;
+    private int plazas;
+    private int transiciones;
+
+    /**
+     * @param matrizIncidencia : int[transiciones][plazas]
+     */
+    public Rdp(int[][] matrizIncidencia, int[] estadoInicial) {
+        matriz = matrizIncidencia;
+        estado = estadoInicial;
+
+        plazas = estado.length;
+        transiciones = matrizIncidencia.length;
+    }
+
+    /**
+     * @param transicion : Numero de transicion empezando desde 1
+     */
+    public boolean Disparar(int transicion) {
+        try {
+            if (IsSensibilizada(transicion)) {
+
+                for (int i = 0; i < plazas; i++) {
+                    estado[i] += matriz[transicion - 1][i];
+                }
+
+                return true;
+            }
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+            System.err.println(
+                    "Numero de plazas en matriz de incidencia no coincide con numero de plazas en el estado incial");
+        }
+
+        return false;
+    }
+
+    public boolean[] GetTransicionesSensibilizadas() {
+        boolean[] transicionesSensibilizadas = new boolean[transiciones];
+
+        for (int i = 0; i < transiciones; i++) {
+            transicionesSensibilizadas[i] = IsSensibilizada(i);
+        }
+
+        return transicionesSensibilizadas;
+    }
+
+    private boolean IsSensibilizada(int transicion) {
+        for (int i = 0; i < plazas; i++) {
+            // Compara la cantidad de tokens por la cantidad de necesarios por esta plaza
+            // Si la suma es menos que 0 no esta sensibilizada
+            if (matriz[transicion][i] + estado[i] < 0) {
+                // No hay tokens necesarios para sensibilizar transición
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
