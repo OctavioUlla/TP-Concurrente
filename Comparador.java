4d4f7d975075a934ec7018f2f68524cae0efc02b
Fermin Verdolini <ferminverdolini@gmail.com>
2023-02-03 10:57:24 -0300

    Arreglo en la contabilizacion de marcados

--- Comparador.java
+++ Comparador.java
@@ -7,6 +7,7 @@ import java.util.Set;
 public class Comparador {
 
     public static void main(String[] args) {
+        final int cantidadDeDisparos = 1000;
         int[][] matrizIncidenciaConDeadlock = {
                 { 1, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, -1, 0, 0, 0 },
                 { -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, -1, 0, 0 },
@@ -44,29 +45,36 @@ public class Comparador {
         Rdp rdpSinDeadlock = new Rdp(matrizIncidenciaSinDeadlock, estadoInicialSinDeadlock);
 
         System.out.println("Red de Petri Sin desbloquear:");
-        Correr(rdpConDeadlock, 1000);
+        Correr(rdpConDeadlock, cantidadDeDisparos);
 
         System.out.println("Red de Petri Desbloqueada:");
-        Correr(rdpSinDeadlock, 1000);
+        Correr(rdpSinDeadlock, cantidadDeDisparos);
     }
 
     public static void Correr(Rdp rdp, int cantidadDeDisparos) {
         int tokensProceso = 0;
-        int[] estadoProceso = new int[10];
-        Set<int[]> marcadosProceso = new HashSet<int[]>();
+        List<Integer> estadoProceso = new ArrayList<>();
+        //int[] estadoProceso = new int[10];
+        Set<List<Integer>> marcadosProceso = new HashSet<List<Integer>>();
         List<Integer> transicionesSensibilizadas = new ArrayList<>();
 
-        for (int j = 0; j < 10; j++) {
-            tokensProceso += rdp.GetEstado()[j];
-            // estadoProceso[j] = rdp.GetEstado()[j];
+        for (int j = 0; j < 10; j++) 
+        { 
+            if(j!=7)
+            {
+                tokensProceso += rdp.GetEstado()[j];
+                estadoProceso.add(rdp.GetEstado()[j]);
+            }            
         }
-        marcadosProceso.add(rdp.GetEstado());
+        marcadosProceso.add(estadoProceso);
+        estadoProceso.clear();
 
-        for (int i = 0; i < cantidadDeDisparos; i++) {
-
+        for (int i = 0; i < cantidadDeDisparos; i++) 
+        {
             transicionesSensibilizadas = rdp.GetTransicionesSensibilizadas();
 
-            if (transicionesSensibilizadas.size() == 0) {
+            if (transicionesSensibilizadas.size() == 0) 
+            {
                 cantidadDeDisparos = i;
                 System.out.println("Se ha detectado un Deadlock \ncantidad de disparos realizados:" + i + "\n");
                 break;
@@ -75,11 +83,16 @@ public class Comparador {
             Collections.shuffle(transicionesSensibilizadas);
             rdp.Disparar(transicionesSensibilizadas.get(0));
 
-            for (int j = 0; j < 10; j++) {
-                tokensProceso += rdp.GetEstado()[j];
-                // estadoProceso[j] = rdp.GetEstado()[j];
+            for (int j = 0; j < 10; j++) 
+            { 
+                if(j!=7)
+                {
+                    tokensProceso += rdp.GetEstado()[j];
+                    estadoProceso.add(rdp.GetEstado()[j]);
+                }            
             }
-            marcadosProceso.add(rdp.GetEstado());
+            marcadosProceso.add(estadoProceso);
+            estadoProceso.clear();
         }
 
         System.out.println(
