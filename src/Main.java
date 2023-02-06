3883f9e1239e5d3397ecf2aa3e0617d331c0251f
SrNandu <octaula@gmail.com>
2023-02-06 02:11:54 -0300

    ImportadorFactory en Main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import Importador.IImportador;
-import Importador.ImportadorPIPE;
+import Importador.ImportadorFactory;
+import Importador.TipoImportador;
 import Main.Rdp;
 
 public class Main {
 
     public static void main(String[] args) {
-        IImportador importador = new ImportadorPIPE();
+        ImportadorFactory importadorFactory = new ImportadorFactory();
+        IImportador importador = importadorFactory.GetImportador(TipoImportador.PIPE);
 
         Rdp rdp = importador.Importar("./RedesDePetri/Red de petri sin deadlock.xml");
     }
