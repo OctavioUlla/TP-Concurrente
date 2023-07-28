e13b14658a2f5efb2c3e7debbdf121d2fc9e0c2d
Octavio Ulla <octaula@gmail.com>
2023-07-27 23:49:55 -0300

    Politica balanceada creada pero no implementada

---
+++ src/Politicas/PoliticaBalanceada.java
@@ -0,0 +1,13 @@
+package Politicas;
+
+import java.util.List;
+
+public class PoliticaBalanceada implements IPolitica {
+
+    @Override
+    public String getProximaTransicion(List<String> transicionesSensibilizadas) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getProximaTransicion'");
+    }
+
+}
