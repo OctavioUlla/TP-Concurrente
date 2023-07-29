5c648d2cecdb9217fe068fdf44ac496d9ee58845
Octavio Ulla <octaula@gmail.com>
2023-07-28 21:19:55 -0300

    Prueba monitor

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
 import Importador.IImportador;
 import Importador.ImportadorFactory;
 import Importador.TipoImportador;
+import Main.Monitor;
 import Main.Rdp;
+import Politicas.PoliticaPrimera;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ImportadorFactory importadorFactory = new ImportadorFactory();
         IImportador importador = importadorFactory.getImportador(TipoImportador.PIPE);
 
         Rdp rdp = importador.importar("./RedesDePetri/Red de petri sin deadlock.xml");
-        rdp.disparar("T1");
+        Monitor monitor = new Monitor(rdp, new PoliticaPrimera());
+        monitor.dispararTransicion("T1");
+        System.out.println("Transicion T1 Disparada");
+        monitor.dispararTransicion("T2");
+        System.out.println("Transicion T2 Disparada");
+        monitor.dispararTransicion("T5");
+        System.out.println("Transicion T5 Disparada");
     }
 }
